b3254fe74a3a9b2fa6dca3169d5dd91bb8630ab4
anniiisssaaaa <anisafebriyanwulandari@gmail.com>
2024-02-28 10:54:12 +0700

    apa aja

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -18,11 +18,37 @@
               <li><a class="dropdown-item" href="{{url('post/list')}}">{{__('Data Post')}}</a></li>
             </ul>
           </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="{{url('post/artikel')}}">{{_('Artikel')}}</a>
+          </li>
         </ul>
-        <form class="d-flex" role="search">
+        <ul class="navbar-nav ms-auto">
+          @auth
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="a" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Wellcome back, {{ auth()->user()->name }}
+            </a>
+
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="/post/dashboard"> <i class="bi bi-layout-text-sidebar-reverse"></i> My Dashboard</a><li>
+                <li><hr class="dropdown-divider"></li>
+                <li>
+                  <form action="/post/logout" method="post">
+                  @csrf 
+                  <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-right"></i>Logout</button>
+                  </form>
+                </li>
+            </ul>
+          </li>
+          @else
+        <li class="nav-item justify-content-end">
+          <a class="nav-link active" aria-current="page" href="{{ url('post/login')}}"><i class="bi bi-box-arrow-right"></i>{{__('Login')}}</a>
+        </li>
+        @endauth
+        {{-- <form class="d-flex" role="search">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        </form> --}}
       </div>
     </div>
   </nav>
